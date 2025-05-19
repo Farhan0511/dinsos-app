fa7d9f1b4fef4b731abf64036e2f36ca0ebc1ada
Farhan0511 <programingsukses.com>
2025-05-19 10:29:45 +0700

    Create basic login view with form and layout using Bootstrap

---
+++ resources/views/auth/pages/login.blade.php
@@ -0,0 +1,57 @@
+@extends('auth.main')
+
+@section('title', 'Login')
+@section('content')
+    <div class="container-xxl position-relative bg-white d-flex p-0">
+        <!-- Spinner Start -->
+        <div id="spinner"
+            class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Sign In Start -->
+        <div class="container-fluid">
+            <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh;">
+                <!-- Gambar di sebelah kiri -->
+                <div class="col-12 col-md-6 d-none d-md-block">
+                    <div class="text-center">
+                        <img src="{{ asset('views/image/disabilitas.jpg') }}" alt="Login Image" class="img-fluid"
+                            style="max-height: 500px;">
+                    </div>
+                </div>
+
+                <!-- Form Login di sebelah kanan -->
+                <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                    <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h3>Login</h3>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                            <label for="floatingInput">Email address</label>
+                        </div>
+                        <div class="form-floating mb-4">
+                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                            <label for="floatingPassword">Password</label>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between mb-4">
+                            <div class="form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <a href="">Forgot Password</a>
+                        </div>
+                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
+                        <p class="text-center mb-0">Don't have an Account? <a href="{{ route('register') }}">Sign Up</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sign In End -->
+    </div>
+@endsection
---
+++ resources/views/auth/pages/register.blade.php
@@ -0,0 +1,5 @@
+@extends('auth.main')
+
+@section('content')
+<h2>hello</h2>
+@endsection

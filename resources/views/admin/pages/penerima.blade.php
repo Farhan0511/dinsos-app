2e1f3503dc235425dbeded3b529898415a38af76
Farhan0511 <programingsukses.com>
2025-05-29 21:22:09 +0700

    add : menampilkan pendaftar yang sudah di verifikasi

--- resources/views/admin/pages/penerima.blade.php
+++ resources/views/admin/pages/penerima.blade.php
@@ -8,62 +8,36 @@
             <div class="page-header">
                 <h3 class="fw-bold mb-3">Data Penerima</h3>
             </div>
-            <div class=" w-100">
+            <div class="w-100">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
                             <div class="card-title">Data Penerima</div>
                         </div>
                         <div class="card-body">
-                            <div class="input-group">
-                                <div class="input-group-prepend">
-                                    <button type="submit" class="btn btn-search pe-1">
-                                    </button>
-                                </div>
-                                <input type="text" placeholder="Search ..." class="form-control" />
-                            </div>
-                            <table class="table mt-3">
+                            <input type="text" placeholder="Search ..." class="form-control mb-3" />
+                            <table class="table table-striped">
                                 <thead>
                                     <tr>
-                                        <th scope="col">No</th>
-                                        <th scope="col">Nama</th>
-                                        <th scope="col">Email</th>
-                                        <th scope="col">Alamat</th>
-                                        <th scope="col">Jenis Kelamin</th>
-                                        <th scope="col">No Telepon</th>
+                                        <th>No</th>
+                                        <th>Nama</th>
+                                        <th>Email</th>
+                                        <th>Alamat</th>
+                                        <th>Jenis Kelamin</th>
+                                        <th>No Telepon</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Mark</td>
-                                        <td>mark@example.com</td>
-                                        <td>Jl. Merdeka No.1</td>
-                                        <td>Laki-laki</td>
-                                        <td>081234567890</td>
-                                        <td>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Jacob</td>
-                                        <td>jacob@example.com</td>
-                                        <td>Jl. Sudirman No.2</td>
-                                        <td>Perempuan</td>
-                                        <td>081298765432</td>
-                                        <td>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Larry the Bird</td>
-                                        <td>larry@example.com</td>
-                                        <td>Jl. Pahlawan No.3</td>
-                                        <td>Laki-laki</td>
-                                        <td>081200011122</td>
-                                        <td>
-                                        </td>
-                                    </tr>
+                                    @foreach ($penerima as $p)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>{{ $p->nama }}</td>
+                                            <td>{{ $p->email }}</td>
+                                            <td>{{ $p->alamat }}</td>
+                                            <td>{{ $p->jenisKelamin }}</td>
+                                            <td>{{ $p->nomorTelepon }}</td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
                             </table>
 

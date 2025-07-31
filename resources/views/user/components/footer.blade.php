87ecd032fc8ce4c8777ccad5fc1140f364fd19dd
farhan0511 <programingsukses@gmail.com>
2025-07-31 10:02:40 +0700

    sedikit perubahan pada footer

--- resources/views/user/components/footer.blade.php
+++ resources/views/user/components/footer.blade.php
@@ -9,26 +9,31 @@
                     Rehabilitasi Sosial Kota Serang
                 </h1>
                 <span>
-                    Rehabilitasi Sosial Kota Serang berkomitmen memberikan pelayanan dan dukungan kepada penyandang disabilitas, lansia, anak terlantar, dan kelompok rentan lainnya. Kami hadir untuk membangun kehidupan yang lebih mandiri dan bermartabat bagi seluruh masyarakat.
+                    Rehabilitasi Sosial Kota Serang berkomitmen memberikan pelayanan dan dukungan kepada penyandang
+                    disabilitas, lansia, anak terlantar, dan kelompok rentan lainnya. Kami hadir untuk membangun
+                    kehidupan yang lebih mandiri dan bermartabat bagi seluruh masyarakat.
                 </span>
             </div>
 
             <!-- Newsletter -->
             <div class="col-md-6">
-                <h5 class="mb-4">Langganan Informasi</h5>
-                <p>Dapatkan informasi terbaru seputar program rehabilitasi sosial di Kota Serang langsung ke email Anda.</p>
+                <h5 class="mb-4 text-primary">Layanan Pengaduan</h5>
+                <p>Sampaikan keluhan atau masukan Anda terkait program rehabilitasi sosial di Kota Serang melalui
+                    formulir berikut.</p>
                 <div class="position-relative">
                     <input class="form-control bg-transparent w-100 py-3 ps-4 pe-5" type="text"
-                        placeholder="Masukkan email Anda">
+                        placeholder="Tulis pengaduan Anda di sini">
                     <button type="button"
-                        class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Langganan</button>
+                        class="btn btn-primary py-2 px-3 position-absolute top-0 end-0 mt-2 me-2">Kirim</button>
                 </div>
             </div>
+
 
             <!-- Kontak -->
             <div class="col-lg-3 col-md-6">
                 <h5 class="mb-4">Kontak Kami</h5>
-                <p><i class="fa fa-map-marker-alt me-3"></i>Jl. KH. Sulaeman No.20, Kagungan, Kec. Serang, Kota Serang, Banten 42114</p>
+                <p><i class="fa fa-map-marker-alt me-3"></i>Jl. KH. Sulaeman No.20, Kagungan, Kec. Serang, Kota Serang,
+                    Banten 42114</p>
                 <p><i class="fa fa-phone-alt me-3"></i>+62 812 3456 7890</p>
                 <p><i class="fa fa-envelope me-3"></i>rehabsosial@kotaserang.go.id</p>
             </div>
@@ -37,10 +42,10 @@
             <div class="col-lg-3 col-md-6">
                 <h5 class="mb-4">Ikuti Kami</h5>
                 <div class="d-flex">
-                    <a class="btn btn-square rounded-circle me-1" href="#"><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-square rounded-circle me-1" href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square rounded-circle me-1" href="#"><i class="fab fa-youtube"></i></a>
-                    <a class="btn btn-square rounded-circle me-1" href="#"><i class="fab fa-linkedin-in"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href="https://www.instagram.com/farhan_ansyah/"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href="https://www.youtube.com/@Han_Development/videos"><i class="fab fa-youtube"></i></a>
+                    <a class="btn btn-square rounded-circle me-1" href="https://www.linkedin.com/in/farhan-ansyah-182491284/"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>

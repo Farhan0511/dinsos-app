2d9cb73e6add23bffc199bcd95f67bd7cdd4ee61
farhan0511 <programingsukses@gmail.com>
2025-07-31 10:01:33 +0700

    zoom foto if user klik

--- resources/views/user/pages/distribusi.blade.php
+++ resources/views/user/pages/distribusi.blade.php
@@ -1,7 +1,6 @@
 @extends('user.main')
 
 @section('content')
-
     <div class="container mt-4">
         <div class="page-inner">
             <div class="page-header">
@@ -31,8 +30,33 @@
                                                 <td>{{ $p->GetUser->jenisBantuan }}</td>
                                                 <td>
                                                     @if ($p->foto_penyerahan)
-                                                        <img src="{{ asset('uploads/distribusi/images/' . $p->foto_penyerahan) }}" alt="Foto Penyerahan"
-                                                            width="100">
+                                                        <img src="{{ asset('uploads/distribusi/images/' . $p->foto_penyerahan) }}"
+                                                            alt="Foto Penyerahan" width="100" style="cursor: pointer;"
+                                                            data-bs-toggle="modal"
+                                                            data-bs-target="#zoomModal{{ $p->id }}">
+                                                        <!-- Modal -->
+                                                        <div class="modal fade" id="zoomModal{{ $p->id }}"
+                                                            tabindex="-1"
+                                                            aria-labelledby="zoomModalLabel{{ $p->id }}"
+                                                            aria-hidden="true">
+                                                            <div class="modal-dialog modal-dialog-centered modal-lg">
+                                                                <div class="modal-content">
+                                                                    <div class="modal-header">
+                                                                        <h5 class="modal-title"
+                                                                            id="zoomModalLabel{{ $p->id }}">Foto
+                                                                            Penyerahan</h5>
+                                                                        <button type="button" class="btn-close"
+                                                                            data-bs-dismiss="modal"
+                                                                            aria-label="Close"></button>
+                                                                    </div>
+                                                                    <div class="modal-body text-center">
+                                                                        <img src="{{ asset('uploads/distribusi/images/' . $p->foto_penyerahan) }}"
+                                                                            alt="Zoom Foto Penyerahan"
+                                                                            class="img-fluid rounded">
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     @else
                                                         <span class="text-muted">Tidak Ada</span>
                                                     @endif
@@ -46,6 +70,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
 @endsection
